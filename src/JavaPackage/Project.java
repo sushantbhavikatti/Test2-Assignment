69d279c5c680f23356881f4ec2750c787de4fb4a
Miyuuu <Miyuuu@host.docker.internal>
2023-11-25 20:20:18 +0530

    This is my first project in eclipse

---
+++ src/JavaPackage/Project.java
@@ -0,0 +1,15 @@
+package JavaPackage;
+
+public class Project {
+	
+	public class Test {
+
+		public static void main(String[] args) {
+			
+	System.out.println("this is my first class");
+		}
+
+	}
+
+
+}
