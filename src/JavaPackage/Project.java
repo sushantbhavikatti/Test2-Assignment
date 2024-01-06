39394649398efdfa14b36dc843e5ecd86db98550
Miyuuu <Miyuuu@DESKTOP-H6TTHMH>
2024-01-06 09:32:56 +0530

    First Commit

--- src/JavaPackage/Project.java
+++ src/JavaPackage/Project.java
@@ -2,13 +2,21 @@ package JavaPackage;
 
 public class Project {
 	
-	public class Test {
-
-		public static void main(String[] args) {
-			
-	System.out.println("this is my first class");
-		}
-
+	public static void main(String[] args) {
+		
+	
+	
+	char ch = '\u0938';
+	char ch1 = '\u0941';
+	char ch2 = '\u0936';
+	char ch3 = '\u093E';
+	char ch4 =  '\u0971';
+	char ch5 =  '\u0924';
+	//char ch5 = 
+	
+	
+	System.out.println(ch+""+ch1+""+ch2+ch4+""+ch3+""+ch5);
+	
 	}
 
 
