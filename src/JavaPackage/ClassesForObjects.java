37d5b3e8e2498e586818c7ed04352f98007afb03
Miyuuu <Miyuuu@DESKTOP-H6TTHMH>
2024-01-06 20:07:30 +0530

    Classes for objects and Armstrong number example

---
+++ src/JavaPackage/ClassesForObjects.java
@@ -0,0 +1,53 @@
+package JavaPackage;
+
+public class ClassesForObjects {
+
+	public static void main(String[] args) {
+		
+		Human h = new Human();
+		
+		Laptop l = new Laptop();
+		
+		SportMan s = new SportMan();
+		
+		TestCase t = new TestCase();
+	}	
+	
+	}
+	
+	class Human{
+		
+	}
+
+	class Laptop {
+		
+	}
+	
+	class SportMan {
+		
+	}
+	
+	class TestCase {
+		
+	}
+	
+	class Tree {
+		
+	}
+	
+	class Fruit {
+		
+	}
+	
+	class Mark {
+		
+	}
+	
+	class Suit {
+		
+	}
+	
+	class Mango {
+		
+	}
+
