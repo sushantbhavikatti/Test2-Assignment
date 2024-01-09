8698a2f4640115474e3c6dc8686cb74111dd8675
Miyuuu <Miyuuu@DESKTOP-H6TTHMH>
2024-01-09 22:54:22 +0530

    Output of below code

---
+++ src/JavaPackage/Test.java
@@ -0,0 +1,17 @@
+package JavaPackage;
+
+class Test { 
+
+	private static int x; 
+
+	public static void main(String args[]) { 
+
+		System.out.println(fun()); 
+	} 
+
+	static int fun() { 
+
+		return ++x; 
+	} 
+} 										
+
