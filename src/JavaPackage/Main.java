619d2826638e7f5b7b13efb2f46b1ccea1d21bce
Miyuuu <Miyuuu@DESKTOP-H6TTHMH>
2024-01-09 20:53:46 +0530

    Output of below program

---
+++ src/JavaPackage/Main.java
@@ -0,0 +1,14 @@
+package JavaPackage;
+
+class Main { 
+
+	public static void main(String args[]) { 
+
+		System.out.println(fun()); 
+	}  
+
+	static int fun() { 
+
+		return 20; 
+	} 
+} 										
